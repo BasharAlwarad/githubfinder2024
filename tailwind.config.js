8c13c00dbcf3ab422160e32cd15a93d21000bb15
Bashar Alwarad <beelwarad52@gmail.com>
2024-10-16 05:21:41 +0200

    nave & themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   daisyui: {
     themes: [
       {
-        mydarktheme: {
+        myDarkTheme: {
           primary: '#5b21b6',
           secondary: '#fbbf24',
           accent: '#4ade80',
@@ -22,8 +22,42 @@ export default {
         },
       },
       'dark',
+      'cyberpunk',
       'light',
+      'luxury',
+      'synthwave',
     ],
   },
   plugins: [daisyui],
 };
+
+// /** @type {import('tailwindcss').Config} */
+// import daisyui from 'daisyui';
+
+// export default {
+//   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+//   theme: {
+//     extend: {},
+//   },
+//   daisyui: {
+//     themes: [
+//       {
+//         myDarkTheme: {
+//           primary: '#5b21b6',
+//           secondary: '#fbbf24',
+//           accent: '#4ade80',
+//           neutral: '#3d4451',
+//           'base-100': '#1F1F1F',
+//           info: '#3ABFF8',
+//           success: '#36D399',
+//           warning: '#FBBD23',
+//           error: '#F87272',
+//         },
+//       },
+//       'dark',
+//       'cyberpunk',
+//       'light',
+//     ],
+//   },
+//   plugins: [daisyui],
+// };
